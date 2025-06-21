f29e5ae082e00f1066a0894748aa0f9423267ca5
gen <geninokuchi@gmail.com>
2025-06-21 18:29:14 +0900

    Vercel デプロイ用の修正：ルートURL追加、wsgi.py修正

--- blogproject/blogpost/urls.py
+++ blogproject/blogpost/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import BlogList,BlogDetail,BlogCreate,BlogDelete,BlogUpdate
 
 urlpatterns = [
-    path('list/', BlogList.as_view(), name='list'),
+    path('', BlogList.as_view(), name='list'),  # ルートURLをリストビューに
+    path('list/', BlogList.as_view(), name='list_alt'),  # 既存のlist URLも維持
     path('detail/<int:pk>', BlogDetail.as_view(), name='detail'),
     path('create/', BlogCreate.as_view(), name='create'),
     path('delete/<int:pk>', BlogDelete.as_view(), name='delete'),
     path('update/<int:pk>', BlogUpdate.as_view(), name='update'),
 ]
+
